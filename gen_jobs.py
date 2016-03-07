d89cc3e9f227fa5823ec3b688c9bfe9f93ac4039
Alexander Kaurov <akaurov@gmail.com>
2016-03-07 16:35:44 -0600

    job generation

---
+++ gen_jobs.py
@@ -0,0 +1,29 @@
+import numpy as np 
+
+z_list = np.round(np.logspace(0, 3, 16))
+d_list = [0.5, 1.0, 10.0, 100.0, 1000.0, 10000.0]
+x_ion = [2e-4, 1e-3, 1e-2, 1e-1, 0.5, 0.9, 0.99]
+
+#python primary_electron_de.py 19.0 100.0 0.1 VFKY1996 RBEQ RBEQ
+
+
+for i in range(len(x_ion)):
+    for j in range(len(d_list)):
+        f1=open('jobs/'+str(x_ion[i])+'_'+str(d_list[j])+'.sh', 'w')
+        temp = '''#!/bin/bash
+#SBATCH --job-name=radiator'''+str(x_ion[i])+'_'+str(d_list[j])+'''
+#SBATCH --output=/home/kaurov/radiator/jobs/radiator'''+str(x_ion[i])+'_'+str(d_list[j])+'''.out
+#SBATCH --error=/home/kaurov/radiator/jobs/radiator'''+str(x_ion[i])+'_'+str(d_list[j])+'''.err
+#SBATCH --time=23:00:00
+#SBATCH --partition=kicp
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+
+cd /home/kaurov/radiator
+
+
+'''
+        for k in range(len(z_list)):
+            temp+='~/anaconda/bin/python primary_electron_de.py '+str(z_list[k])+' '+str(d_list[j])+' '+str(x_ion[i])+' VFKY1996 RBEQ RBEQ & \n'
+        temp+='wait'
+        f1.write(temp)
